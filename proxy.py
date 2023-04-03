01f1d478090ed307e3eefb6eb8f93da74a823072
Justin Thoreson <thoresonjd@gmail.com>
2023-04-03 15:02:58 -0700

    Proxy class setup + relevant imports

--- proxy.py
+++ proxy.py
@@ -5,6 +5,24 @@ Web Proxy
 :version: 1.0
 """
 
+from urllib.parse import urlparse
+from pathlib import Path
+from socket import *
+import sys
+
+class Proxy():
+    def __init__():
+        pass
+
+    def run():
+        pass
+
+    def receive():
+        pass
+    
+    def send():
+        pass
+
 def main() -> None:
     print('Hello, world!')
 
