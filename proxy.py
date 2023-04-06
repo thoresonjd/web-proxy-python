7072bac9d1325ba775aa50d4cbbafa7a5ade096e
Justin Thoreson <thoresonjd@gmail.com>
2023-04-05 19:02:51 -0700

    Generating response message

--- proxy.py
+++ proxy.py
@@ -14,8 +14,11 @@ HOSTNAME = 'localhost'
 BUF_SZ = 4096
 BACKLOG = 5
 TIMEOUT = 1
+INTRNL_ERR = 500
 HTTP_PORT = 80
 HTTP_VERSION = 'HTTP/1.1'
+HTTP_METHODS = {'GET'}
+HTTP_CODES = {200, 404, INTRNL_ERR}
 DEFAULT_PATH = '/'
 END_L = '\r\n'
 
@@ -26,7 +29,7 @@ class RequestError(ValueError):
         super().__init__(f'Request Error: {message}')
 
 class Proxy():
-    """A proxy that sits between a client and a server."""
+    """A proxy that caches HTTP traffic between clients and servers."""
 
     def __init__(self, port: int) -> None:
         self.listener, self.address = self.start_server(HOSTNAME, port)
@@ -50,26 +53,23 @@ class Proxy():
         
         while True:
             conn, addr = self.listener.accept()
-            client_request = conn.recv(BUF_SZ)
+            client_request = conn.recv(BUF_SZ).decode('UTF-8')
             try:
-                host, port, path = self.parse_request(client_request)
+                method, host, port, path = self.parse_request(client_request)
             except RequestError as e:
                 print(e)
                 conn.close()
                 continue
-            server_request = self.generate_request(host, port, path)
+            server_request = self.generate_request(method, host, port, path)
             print(server_request)
-            try:
-                response = self.send_request(server_request, (host, port))
-            except Exception as e:
-                print(e)
-                conn.close()
-                continue
-            conn.sendall(response)
+            response = self.send_request(server_request, (host, port))
+            if self.status_code(response) not in HTTP_CODES:
+                response = self.generate_response(INTRNL_ERR, 'Internal Server Error', False)
+            conn.sendall(response.encode('UTF-8'))
             conn.close()
 
     @staticmethod
-    def parse_request(request: bytes) -> tuple:
+    def parse_request(request: str) -> tuple:
         """
         Parses and checks validity of incoming request.
         :param request: The initial request received from the client
@@ -77,34 +77,55 @@ class Proxy():
         :raises RequestError: Invalid request
         """
 
-        method, uri, http_version = request.decode('UTF-8').split()
+        method, uri, http_version = request.split()
         uri = urlparse(uri)
-        if not method == 'GET':
-            raise RequestError('Unsupported HTTP method')
+        if method.upper() not in HTTP_METHODS:
+            raise RequestError(f'Unsupported or unrecognized HTTP method: {method}')
         if not uri.hostname:
             raise RequestError('Invalid URI')
         if not http_version == HTTP_VERSION:
             raise RequestError('Unsupported HTTP version')
         port = uri.port if uri.port else HTTP_PORT
         path = uri.path if uri.path else DEFAULT_PATH
-        return uri.hostname, port, path
+        return method, uri.hostname, port, path
 
     @staticmethod
-    def generate_request(host: str, port: int, path: str) -> str:
+    def status_code(response: str) -> int:
+        """Determines the status code of a response."""
+        
+        return int(response.split()[1])
+
+    @staticmethod
+    def generate_request(method: str, host: str, port: int, path: str) -> str:
         """
         Generates a request in HTTP message format.
+        :param method: The HTTP method of the request
         :param host: The host of the server to send the request to
         :param port: The port of the server to send the request to
         :param path: The path of the resource being requested
-        :return: A request in HTTP message format
+        :return: An HTTP request
         """
 
-        return f'GET {path} {HTTP_VERSION}{END_L}' \
+        return f'{method.upper()} {path} {HTTP_VERSION}{END_L}' \
                f'Host: {host}{END_L}' \
                f'Connection: close{END_L*2}'
 
     @staticmethod
-    def send_request(request: str, address: tuple) -> bytes:
+    def generate_response(status_code: int, message: str, hit: bool) -> str:
+        """
+        Generates a response in HTTP message format.
+        :param status_code: The status code of the response
+        :param message: The status message of the response
+        :param hit: True if there was a cache hit, False otherwise
+        :return: An HTTP response
+        """
+
+        return f'{HTTP_VERSION} {status_code} {message}{END_L}' \
+               f'Cache-Hit: {int(hit)}{END_L}' \
+               f'Connection: close{END_L*2}'
+
+    @staticmethod
+    def send_request(request: str, address: tuple) -> str:
         """
         Sends a request to a server.
         :param request: The request to send
@@ -116,17 +137,22 @@ class Proxy():
             server.connect(address)
             server.settimeout(TIMEOUT)
             server.sendall(request.encode('UTF-8'))
-            return server.recv(BUF_SZ)
-            # response = server.recv(BUF_SZ)
-            # return response.decode('UTF-8')
+            return server.recv(BUF_SZ).decode('UTF-8')
+
+def has_valid_args() -> bool:
+    """Checks the validity of command line arguments."""
+
+    return len(sys.argv) == 2 and sys.argv[1].isdigit()
+
 
 def main() -> None:
     """Runs the web proxy program."""
         
-    if len(sys.argv) != 2:
-        print('USAGE: python3 proxy.py <port>')
+    if not has_valid_args():
+        print('USAGE: python3 proxy.py <PORT>')
         exit(1)
-    proxy = Proxy(int(sys.argv[1]))
+    port = int(sys.argv[1])
+    proxy = Proxy(port)
     proxy.run()
 
 if __name__ == '__main__':
